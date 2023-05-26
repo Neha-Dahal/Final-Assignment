a39119271f2d0839d612d6e45edd502920824b6a
Neha Dahal <nehudahal57@gmail.com>
2023-05-26 14:20:19 +0545

    score added

--- index.js
+++ index.js
@@ -12,19 +12,28 @@ let doodlerY = (canvasHeight * 7) / 8 - doodlerHeight;
 
 let doodlerRightImg;
 let doodlerLeftImg;
+let doodlerUpImg;
+let isShooting = false;
 
 //physics
 let velocityX = 0;
 let velocityY = 0; //doodler jump speed
-let initialVelocityY = -8; //starting velocity Y
-let gravity = 0.4;
+let initialVelocityY = -3; //starting velocity Y
+let gravity = 0.1;
 
 //platforms
 let platformArray = [];
 let platformWidth = 60;
 let platformHeight = 18;
 let platformImg;
-let platformsOnScreen = 6;
+let platformsOnScreen = 10;
+
+//score
+let maxScore = 0;
+let score = 0;
+
+//gameover
+let gameOver = false;
 
 const doodler = {
   img: null,
@@ -61,6 +70,9 @@ window.onload = function () {
   doodlerLeftImg = new Image();
   doodlerLeftImg.src = "./images/doodler-left.png";
 
+  doodlerUpImg = new Image();
+  doodlerUpImg.src = "./images/doodler-up.png";
+
   platformImg = new Image();
   platformImg.src = "./images/platform.png";
 
@@ -72,6 +84,9 @@ window.onload = function () {
 
 function update() {
   requestAnimationFrame(update);
+  if (gameOver) {
+    return;
+  }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //doodler
@@ -91,16 +106,17 @@ function update() {
     doodler.width,
     doodler.height
   );
-
+  drawScore();
   //platform
   for (let i = 0; i < platformArray.length; i++) {
     let platform = platformArray[i];
 
-    if (velocityY < 0 && doodler.y < (canvasHeight * 4) / 5) {
+    if (velocityY < 0 && doodler.y < (canvasHeight * 6) / 7) {
       platform.y -= initialVelocityY;
     }
     if (detectCollision(doodler, platform) && velocityY >= 0) {
       velocityY = initialVelocityY;
+      // score += 10;
     }
     ctx.drawImage(
       platform.img,
@@ -115,17 +131,26 @@ function update() {
     platformArray.shift();
     newPlatform();
   }
+  drawScore();
 }
 
 function moveDoodler(e) {
   if (e.code == "ArrowRight" || e.code == "KeyD") {
     //move right
-    velocityX = 4;
+    velocityX = 3;
     doodler.img = doodlerRightImg;
   } else if (e.code == "ArrowLeft" || e.code == "KeyA") {
     //move left
-    velocityX = -4;
+    velocityX = -3;
     doodler.img = doodlerLeftImg;
+  } else if (e.code == "Space" || e.code == "Spacebar") {
+    doodler.img = doodlerUpImg;
+    isShooting = true;
+
+    setTimeout(() => {
+      doodler.img = doodlerRightImg;
+      isShooting = false;
+    }, 300);
   }
 }
 
@@ -139,6 +164,8 @@ function placePlatforms() {
     y: canvasHeight - 50,
     width: platformWidth,
     height: platformHeight,
+    passed: false,
+    touched: false,
   };
   platformArray.push(platform);
 
@@ -150,8 +177,25 @@ function placePlatforms() {
       y: canvasHeight - 75 * i - 150,
       width: platformWidth,
       height: platformHeight,
+      // passed: false,
+      // touched: false,
     };
-    platformArray.push(platform);
+    let overlapping = false;
+    for (let j = 0; j < platformArray.length; j++) {
+      let existingPlatform = platformArray[j];
+      if (
+        Math.abs(platform.x - existingPlatform.x) < platformWidth &&
+        Math.abs(platform.y - existingPlatform.y) < platformHeight
+      ) {
+        overlapping = true;
+        break;
+      }
+    }
+    if (!overlapping) {
+      platformArray.push(platform);
+    } else {
+      i--; // Retry with a new platform position
+    }
   }
 }
 
@@ -163,15 +207,57 @@ function newPlatform() {
     y: -platformHeight,
     width: platformWidth,
     height: platformHeight,
+    passed: false,
+    touched: false,
   };
-  platformArray.push(platform);
+  let overlapping = false;
+  for (let j = 0; j < platformArray.length; j++) {
+    let existingPlatform = platformArray[j];
+    if (
+      Math.abs(platform.x - existingPlatform.x) < platformWidth &&
+      Math.abs(platform.y - existingPlatform.y) < platformHeight
+    ) {
+      overlapping = true;
+      break;
+    }
+  }
+  if (!overlapping) {
+    platformArray.push(platform);
+  } else {
+    return;
+  }
+  // platformArray.push(platform);
 }
 
 function detectCollision(a, b) {
   return (
-    a.x < b.x + b.width && 
+    a.x < b.x + b.width &&
     a.x + a.width > b.x &&
     a.y < b.y + b.height &&
     a.y + a.height > b.y
   );
+}
+function drawScore() {
+  let points = 10;
+
+  for (let i = 0; i < platformArray.length; i++) {
+    let platform = platformArray[i];
+
+    if (detectCollision(doodler, platform)) {
+      // Check if the doodler is jumping on a new platform
+      if (velocityY < 0 && !platform.touched) {
+        score += points;
+        platform.touched = true; // Mark the platform as touched
+      }
+      // Exit the loop if the doodler is on a platform to avoid incrementing score multiple times
+      break;
+    } else if (doodler.y + doodler.height < platform.y && !platform.passed) {
+      score += points;
+      platform.passed = true;
+    }
+  }
+
+  ctx.fillStyle = "black";
+  ctx.font = "24px Arial";
+  ctx.fillText("Score: " + score, 10, 30);
 }

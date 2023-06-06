5838e8494c2fa0d3f8370f9eb1afaea49df104b6
Neha Dahal <nehudahal57@gmail.com>
2023-06-06 13:57:21 +0545

    paths updated

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,9 @@
-const jumpSound = new Audio("../audio/jump.wav");
+const jumpSound = new Audio("./audio/jump.wav");
 
-const fallSound = new Audio("../audio/fall.mp3");
+const fallSound = new Audio("./audio/fall.mp3");
 
-const gunshotSound = new Audio("../audio/gunshot.wav");
+const gunshotSound = new Audio("./audio/gunshot.wav");
 
-const shootOnMonsterSound = new Audio("../audio/shootonmonster.mp3");
+const shootOnMonsterSound = new Audio("./audio/shootonmonster.mp3");
 
-const monsterCrashSound = new Audio("../audio/monster-crash.mp3");
+const monsterCrashSound = new Audio("./audio/monster-crash.mp3");
